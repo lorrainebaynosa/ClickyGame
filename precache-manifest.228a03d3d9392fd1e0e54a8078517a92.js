47683360e5a269196782d82d1ae1f9c5cf59810d
LORRAINE BAYNOSA <you@example.com>
2019-08-05 22:35:00 -0700

    Updates

---
+++ precache-manifest.228a03d3d9392fd1e0e54a8078517a92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c13c4e1da557fe4c7076a9b4561c08b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "d1413148b061d1e48a5e",
+    "url": "/ClickyGame/static/css/main.65eb51c2.chunk.css"
+  },
+  {
+    "revision": "30ef4bba0e85f28931a0",
+    "url": "/ClickyGame/static/js/2.b52a1d5d.chunk.js"
+  },
+  {
+    "revision": "d1413148b061d1e48a5e",
+    "url": "/ClickyGame/static/js/main.92d478b8.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
